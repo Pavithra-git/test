5aefbb2926d081372d77fcba3920d92a840857ba
Pavithra <tsbpavithra@gmail.com>
2018-09-11 07:30:39 -0700

    Added a "HI" Message

--- TestProject/src/com/icicibank/loans/carloans/SecondClass.java
+++ TestProject/src/com/icicibank/loans/carloans/SecondClass.java
@@ -3,7 +3,7 @@ package com.icicibank.loans.carloans;
 public class SecondClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("HI");
 
 	}
 
